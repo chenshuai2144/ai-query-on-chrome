8796d11224d278be08bdeb520e3b84c9df7c9a74
期贤 <qixian.cs@antgroup.com>
2023-10-12 14:14:09 +0800

    init

--- script/getIssue.js
+++ script/getIssue.js
@@ -1,16 +1,21 @@
 ﻿const { Octokit } = require('octokit');
 const fs = require('fs');
 const prettier = require('prettier');
+const fetch = require('node-fetch');
+const glob = require('glob');
+const { join } = require('path');
+
 const github = new Octokit({
   debug: process.env.NODE_ENV === 'development',
   auth: process.env.GITHUB_TOKEN || process.env.GITHUB_AUTH,
+  request: { fetch },
 });
 
 const getIssue = async (owner, repo) => {
   const iterator = github.paginate.iterator(github.rest.issues.listForRepo, {
     owner: owner || 'ant-design',
     repo: repo || 'ant-design',
-    per_page: 10,
+    per_page: 100,
     state: 'closed',
     since: '2023-01-01T00:00:00Z',
   });
@@ -24,7 +29,11 @@ const getIssue = async (owner, repo) => {
   // iterate through each response
   for await (const { data: issues } of iterator) {
     for (const issue of issues) {
-      if (!issue.pull_request && issue.state !== 'open') {
+      if (
+        !issue.pull_request &&
+        issue.state !== 'open' &&
+        issue.title.length > 12
+      ) {
         console.log('Issue #%d: %s', issue.number, issue.title);
         const comments = await github.rest.issues.listComments({
           repo: repo || 'ant-design',
@@ -79,4 +88,18 @@ ${text}
 };
 
 // getIssue('ant-design', 'ant-design').catch(console.error);
-getIssue('umijs', 'umi').catch(console.error);
+// getIssue('umijs', 'umi').catch(console.error);
+// getIssue('ant-design', 'pro-components').catch(console.error);
+// getIssue('ant-design', 'ant-design-pro').catch(console.error);
+
+const getAllMdList = () => {
+  const fileList = glob
+    .globSync(__dirname + `/../**/**/*.md`)
+    .map((item) => {
+      return item.replace(join(__dirname, '..'), '');
+    })
+    .map((item) => {
+      return fs.readFileSync(join(__dirname, '..', item), 'utf-8');
+    });
+  return fileList;
+};

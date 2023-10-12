9c7847a421045e1769bd712b7dde82bd9f7d9636
陈帅 <qixian.cs@outlook.com>
2023-10-12 15:52:22 +0800

    init (#1)

--- script/getIssue.js
+++ script/getIssue.js
@@ -94,12 +94,103 @@ ${text}
 
 const getAllMdList = () => {
   const fileList = glob
-    .globSync(__dirname + `/../**/**/*.md`)
+    .globSync(__dirname + `/../**/**/*.md`, {
+      ignore: ['**/node_modules/**', 'README.md'],
+    })
     .map((item) => {
       return item.replace(join(__dirname, '..'), '');
     })
     .map((item) => {
-      return fs.readFileSync(join(__dirname, '..', item), 'utf-8');
+      return {
+        content: fs.readFileSync(join(__dirname, '..', item), 'utf-8'),
+        path: item,
+      };
     });
   return fileList;
 };
+
+const { QdrantClient } = require('@qdrant/js-client-rest');
+
+// TO connect to Qdrant running locally
+const client = new QdrantClient({ url: 'http://127.0.0.1:6333' });
+
+const prepareData = async () => {
+  const collectionName = 'test_collection';
+
+  let result = await client.getCollections();
+
+  const collectionNames = result.collections.map(
+    (collection) => collection.name
+  );
+
+  if (collectionNames.includes(collectionName)) {
+    await client.deleteCollection(collectionName);
+  }
+
+  await client.createCollection(collectionName, {
+    vectors: {
+      size: 768,
+      distance: 'Cosine',
+    },
+    optimizers_config: {
+      default_segment_number: 2,
+    },
+    replication_factor: 2,
+  });
+  result = await client.getCollections();
+
+  console.log('List of collections:', result.collections);
+
+  await client.createPayloadIndex(collectionName, {
+    field_name: 'text',
+    field_schema: 'keyword',
+    wait: true,
+  });
+
+  await client.createPayloadIndex(collectionName, {
+    field_name: 'url',
+    field_schema: 'keyword',
+    wait: true,
+  });
+
+  const list = getAllMdList();
+  let index = 0;
+  let subIndex = 1;
+  for await (const item of list) {
+    const lines = item.content.split('#');
+    const points = [];
+    for await (const line of lines) {
+      if (line.length < 10) {
+        continue;
+      }
+      const vector = await fetch('http://127.0.0.1:5000/encode', {
+        method: 'POST',
+        body: JSON.stringify({
+          text: line,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((res) => res.text())
+        .then((res) => eval(res));
+
+      points.push({
+        id: index * 10 + subIndex,
+        vector,
+        payload: {
+          text: line,
+          url: item.path,
+        },
+      });
+      subIndex++;
+    }
+    await client.upsert(collectionName, {
+      points: points,
+    });
+
+    index++;
+  }
+};
+
+prepareData();

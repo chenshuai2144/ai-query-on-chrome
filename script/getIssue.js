b5783ad0e9edc3520bfddcca6ba6125f44316a00
期贤 <qixian.cs@antgroup.com>
2023-10-12 09:46:13 +0800

    初始化issue数据

---
+++ script/getIssue.js
@@ -0,0 +1,82 @@
+﻿const { Octokit } = require('octokit');
+const fs = require('fs');
+const prettier = require('prettier');
+const github = new Octokit({
+  debug: process.env.NODE_ENV === 'development',
+  auth: process.env.GITHUB_TOKEN || process.env.GITHUB_AUTH,
+});
+
+const getIssue = async (owner, repo) => {
+  const iterator = github.paginate.iterator(github.rest.issues.listForRepo, {
+    owner: owner || 'ant-design',
+    repo: repo || 'ant-design',
+    per_page: 10,
+    state: 'closed',
+    since: '2023-01-01T00:00:00Z',
+  });
+
+  if (!fs.existsSync(__dirname + `/../issues/${owner}/${repo}`)) {
+    fs.mkdirSync(__dirname + `/../issues/${owner}/${repo}`, {
+      recursive: true,
+    });
+  }
+
+  // iterate through each response
+  for await (const { data: issues } of iterator) {
+    for (const issue of issues) {
+      if (!issue.pull_request && issue.state !== 'open') {
+        console.log('Issue #%d: %s', issue.number, issue.title);
+        const comments = await github.rest.issues.listComments({
+          repo: repo || 'ant-design',
+          owner: owner || 'ant-design',
+          issue_number: issue.number,
+        });
+        if (comments.data.length < 1) {
+          continue;
+        }
+        const text = comments.data
+          .map((item) => {
+            if (item.user.login.includes('github-actions')) {
+              return '';
+            }
+            return `
+## ${item.user.login}
+
+${item.body}
+          `;
+          })
+          .join('\n\n');
+        fs.writeFileSync(
+          __dirname + `/../issues/${owner}/${repo}/${issue.number}.md`,
+          await prettier
+            .format(
+              `
+# ${issue.title}
+        
+${issue.labels
+  .map((item) => {
+    if (typeof item === 'string') {
+      return item;
+    }
+    return item.name;
+  })
+  .map((item) => `\`${item}\``)}
+
+${issue.body}
+
+
+${text}
+        `,
+              {
+                filepath: 'foo.md',
+              }
+            )
+            .then((res) => res.replace(/\n\n\n/g, '\n\n'))
+        );
+      }
+    }
+  }
+};
+
+// getIssue('ant-design', 'ant-design').catch(console.error);
+getIssue('umijs', 'umi').catch(console.error);
